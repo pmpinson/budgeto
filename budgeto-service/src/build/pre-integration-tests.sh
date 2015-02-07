ed4da1f5d281679f12b1e5a3bbedc7759a71a0a2
pmpinson <pmpinson@users.noreply.github.com>
2015-02-06 20:23:28 -0500

    Update pre-integration-tests.sh

--- budgeto-service/src/build/pre-integration-tests.sh
+++ budgeto-service/src/build/pre-integration-tests.sh
@@ -5,9 +5,9 @@
 # ---------------------------------------------------------------------
 #
 
-# docker run -d --name budgeto-mongo-server-autotest -p 27018:27017 mongo
-MONGO_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' dev-jenkins-server)
-echo 'mongo adress' && echo $MONGO_ADDRESS
-sed -i 's|#mongo.srv.host=?.?.?.?|mongo.srv.host=${MONGO_ADDRESS}|g' etc/config.properties
+docker run -d --name budgeto-mongo-server-autotest -p 27018:27017 mongo
+MONGO_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' budgeto-mongo-server-autotest)
+echo 'mongo adress : '$MONGO_ADDRESS
+sed -i 's|#mongo.srv.host=?.?.?.?|mongo.srv.host='$MONGO_ADDRESS'|g' etc/config.properties
 sed -i 's|#mongo.srv.port=27017|mongo.srv.port=27017|g' etc/config.properties
 cat etc/config.properties
