b2464b98ba89fd35ca55d82532bf5f3320500fbc
ppinson <pierre-marie.pinson@tritondigital.com>
2015-02-09 11:40:24 -0500

    correct push error on tmp script modification

--- budgeto-service/src/build/pre-integration-tests.sh
+++ budgeto-service/src/build/pre-integration-tests.sh
@@ -7,21 +7,20 @@
 
 # start
 docker run -d --name budgeto-mongo-server-autotest -p 27018:27017 mongo
-#MONGO_ADDRESS=127.0.0.1
-#$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' budgeto-mongo-server-autotest)
-#MONGO_PORT=27018
+MONGO_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' budgeto-mongo-server-autotest)
+MONGO_PORT=27018
 
 # update confi ip / port
-#sed -i 's|#mongo.srv.host=?.?.?.?|mongo.srv.host='$MONGO_ADDRESS'|g' etc/config.properties
-#sed -i 's|#mongo.srv.port=27017|mongo.srv.port='$MONGO_PORT'|g' etc/config.properties
+sed -i 's|#mongo.srv.host=?.?.?.?|mongo.srv.host='$MONGO_ADDRESS'|g' etc/config.properties
+sed -i 's|#mongo.srv.port=27017|mongo.srv.port='$MONGO_PORT'|g' etc/config.properties
 
 # wait mongo ready
-#for i in `seq 1 20`;
-#do
-#        if wget http://$MONGO_ADDRESS:$MONGO_PORT/ ; then
-#                exit 0
-#        fi;
-#        sleep 3
-#done
-#exit 1
-#echo "$(date) - end"
+for i in `seq 1 20`;
+do
+        if wget http://$MONGO_ADDRESS:$MONGO_PORT/ ; then
+                exit 0
+        fi;
+        sleep 3
+done
+exit 1
+echo "$(date) - end"
