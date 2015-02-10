84bd0eec4f9398a99be2f180581d8b676398ec3a
pmpinson <pmpinson@users.noreply.github.com>
2015-02-09 22:49:39 -0500

    #17 implement test

--- budgeto-service/src/test/java/org/pmp/budgeto/common/controller/ControllerDispatcherConfigTest.java
+++ budgeto-service/src/test/java/org/pmp/budgeto/common/controller/ControllerDispatcherConfigTest.java
@@ -1,5 +1,8 @@
 package org.pmp.budgeto.common.controller;
 
+import org.pmp.budgeto.test.TestTools;
+import org.pmp.budgeto.common.tools.DateTools;
+import java.text.SimpleDateFormat;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +11,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,6 +30,15 @@ public class ControllerDispatcherConfigTest {
         Assertions.assertThat(ControllerDispatcherConfig.class.isAnnotationPresent(EnableWebMvc.class)).isTrue();
         Assertions.assertThat(ControllerDispatcherConfig.class.isAnnotationPresent(ComponentScan.class)).isTrue();
         Assertions.assertThat(ControllerDispatcherConfig.class.getAnnotation(ComponentScan.class).basePackages()).containsExactly("org.pmp.budgeto.common.controller");
+    }
+    
+    @Test
+    public void jacksonFactory() {
+        Jackson2ObjectMapperFactoryBean factory = controllerDispatcherConfig.jacksonFactory()
+        
+        Assertions.assertThat(factory).isNotNull();
+        Assertions.assertThat(TestTools.get(TestTools.get(factory, dateFormat, SimpleDateFormat.class), pattern, String.class).isEqualsTo(DateTools.PATTERN_DATETIMEMS_WITHZONE);
+        Assertions.assertThat(TestTools.get(factory, features, Map.class.get(SerializationFeature.INDENT_OUTPUT))).isEqualsTo(true);
     }
 
 }
