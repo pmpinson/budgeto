6fd18979043b7dec6a01cd76da3c143ff90107e3
pmpinson <pmpinson@users.noreply.github.com>
2015-02-09 21:42:59 -0500

    #17 configure jackson datetime conversion

--- budgeto-service/src/main/java/org/pmp/budgeto/common/controller/ControllerDispatcherConfig.java
+++ budgeto-service/src/main/java/org/pmp/budgeto/common/controller/ControllerDispatcherConfig.java
@@ -1,8 +1,15 @@
 package org.pmp.budgeto.common.controller;
 
+import org.pmp.budgeto.common.tools.DateTools;
+
+import om.fasterxml.jackson.datatype.joda.JodaModule;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * to configure the rest app servlet dispatcher
@@ -10,6 +17,22 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"org.pmp.budgeto.common.controller"})
-public class ControllerDispatcherConfig {
+public class ControllerDispatcherConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperFactoryBean factory = new Jackson2ObjectMapperFactoryBean();
+        factory.setIndentOutput(true);
+        factory.setSimpleDateFormat(DateTools.PATTERN_DATETIMEMS_WITHZONE);
+        factory.afterPropertiesSet();
+        
+        ObjectMapper objectMapper = factory.getObject();
+        objectMapper.registerModule(new JodaModule());
+        
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        
+        converters.add(converter);
+    }
 
 }
