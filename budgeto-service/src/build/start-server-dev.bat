cd0570a224e60a33c149475ca0f43e8bb6b92dcd
Pierre-Marie Pinsons <pmpinson@gmail.com>
2015-02-14 18:53:05 -0500

    #82 configure to run partially on windows

---
+++ budgeto-service/src/build/start-server-dev.bat
@@ -0,0 +1,7 @@
+rem
+rem ---------------------------------------------------------------------
+rem Start application in tomcat8 (port 9001) with cargo. run with jvm debug mode on port 8998m not wait
+rem ---------------------------------------------------------------------
+rem
+
+mvn clean package cargo:run -Pcargo-dev -Dmaven.test.skip=true
