e6aedc488a92ef20dff08fb7d7222faf235d73cc
Pierre-Marie Pinsons <pmpinson@gmail.com>
2015-02-21 12:16:32 -0500

    #102 bower integration

--- budgeto-ui/Gruntfile.js
+++ budgeto-ui/Gruntfile.js
@@ -5,8 +5,8 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          compress: false,
-          yuicompress: false,
+          compress: true,
+          yuicompress: true,
           optimization: 2
         },
         files: {
@@ -14,6 +14,31 @@ module.exports = function(grunt) {
         }
       }
     },
+      bower: {
+        install: {
+            options: {
+                bowerOptions: {
+                    production: true
+                }
+            }
+        }
+      },
+      bower_concat:{
+          all: {
+              dest: 'lib/libs.js',
+              cssDest: 'lib/libs.css',
+              bowerOptions: {
+                relative: true
+              }
+            }
+      },
+      uglify: {
+          js: {
+            files: {
+              'app/js/libs.js': ['lib/libs.js']
+            }
+          }
+        },
     watch: {
        less: {
          files: ['app/less/*.less'], // which files to watch
@@ -34,8 +59,11 @@ module.exports = function(grunt) {
 
 
     grunt.loadNpmTasks('grunt-bg-shell');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('compile', ['less']);
+  grunt.registerTask('compile', ['less', 'bower', 'bower_concat', 'uglify']);
   grunt.registerTask('server', ['compile','bgShell:server']);
 
 //  grunt.registerTask('server', 'install the backend and frontend dependencies', startServer);
