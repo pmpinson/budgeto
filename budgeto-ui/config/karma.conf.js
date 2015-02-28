027e49a5aecc846868588d507a0ef2d3e01f4410
Pierre-Marie Pinsons <pmpinson@gmail.com>
2015-02-28 14:11:16 -0500

    #113 add js code coverage

--- budgeto-ui/config/karma.conf.js
+++ budgeto-ui/config/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: '../',
 
 
     // frameworks to use
@@ -32,13 +32,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+          'app/app.js': ['coverage'],
+          'app/components/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'target/coverage/'
+    },
 
 
     // web server port
