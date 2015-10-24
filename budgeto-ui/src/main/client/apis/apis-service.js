0b620efe46655dea94842e895cb008e7585a242b
ppinson <pierre-marie.pinson@tritondigital.com>
2015-10-23 23:41:04 -0400

    review comment

--- budgeto-ui/src/main/client/apis/apis-service.js
+++ budgeto-ui/src/main/client/apis/apis-service.js
@@ -1,5 +1,5 @@
 /**
- * provider to manage loading of application
+ * provider to manage definition of apis
  */
 class ApisService {
 
@@ -10,6 +10,10 @@ class ApisService {
         this.apis = [];
     }
 
+    /**
+     * load apis definition
+     * @returns {*}
+     */
     load() {
         this.$log.debug('ApisService', 'call api to get all available apis', this.budgetoRestApiURL);
 
@@ -22,6 +26,10 @@ class ApisService {
         });
     }
 
+    /**
+     * load result of apis definitions
+     * @param data
+     */
     loadApis(data) {
         for (var key in data.links) {
             if (data.links[key].rel !== 'self') {
@@ -31,14 +39,29 @@ class ApisService {
         this.$log.debug('ApisService', 'available apis', this.apis);
     }
 
+    /**
+     * get all apis
+     * @returns {Array}
+     */
     all() {
         return this.apis;
     }
 
+    /**
+     * get one api by is name
+     * @param rel
+     * @returns {*}
+     */
     get(rel) {
         return this.getLink(rel, this.apis);
     }
 
+    /**
+     * get link of an api by name
+     * @param rel
+     * @param links
+     * @returns {*}
+     */
     getLink(rel, links) {
         for (var key in links) {
             if (links[key].rel === rel) {
