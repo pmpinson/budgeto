3993899dd5c40566b4e70a2aaa1fa1c787283a34
ppinson <pierre-marie.pinson@tritondigital.com>
2015-10-23 23:50:39 -0400

    reorder code

--- budgeto-ui/src/main/client/home/home.js
+++ budgeto-ui/src/main/client/home/home.js
@@ -3,12 +3,13 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
+import loading from '../loading/loading';
+import apis from '../apis/apis';
+
 import homeRoute from './home-route';
 homeRoute.$inject = ['$stateProvider'];
 import homeController from './home-controller';
 homeController.$inject = ['$log', '$state', 'loadingService', 'apisService'];
-import loading from '../loading/loading';
-import apis from '../apis/apis';
 
 /**
  * definition of angular home module
