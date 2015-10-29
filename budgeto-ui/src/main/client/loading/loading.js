ba6ac0a653afca2c5d00f3dbd1b3721e0d832820
ppinson <pierre-marie.pinson@tritondigital.com>
2015-10-28 23:41:37 -0400

    rename angular module

--- budgeto-ui/src/main/client/loading/loading.js
+++ budgeto-ui/src/main/client/loading/loading.js
@@ -2,6 +2,6 @@ import angular from 'angular';
 
 import loadingService from './loading-service';
 
-export default angular.module('budgeto.loading', [])
+export default angular.module('tools.loading', [])
     .service('loadingService', ['$log', '$q', '$injector', loadingService])
     .name;
